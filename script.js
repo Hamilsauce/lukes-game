01ad4a2308c39f173b205927b34cbee5789304f5
hamilsauce <jacob.w.hamilton@outlook.com>
2025-09-04 17:54:47 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import ham from 'https://hamilsauce.github.io/hamhelper/hamhelper1.0.0.js';
 import { dom } from 'https://hamilsauce.github.io/hamhelper/modules/index.js';
-// const { template, utils, dom } = ham;
 import { randInt } from './lib/randomizer.js';
 import { GameListModel } from './GameListModel.js';
 import { mockSlots1 } from './mock-slot-list.js';
@@ -11,47 +10,19 @@ const appBody = app.querySelector('#app-body')
 const gameGrid = appBody.querySelector('#game-grid')
 const randomBtn = appBody.querySelector('#random-button')
 
-
-// const flow = createFlowSequencer()
-
 let gameList
+
 setTimeout(() => {
-  
   gameList = new GameListModel(gameGrid.children.length)
   window.gameList = gameList
-  // const lowestValid = gameList.findLowestValid(5)
-  // console.warn('lowestValid', lowestValid)
-  
-}, 0)
+}, 0);
 
-let cnt = 0
-// const intId = setInterval(() => {
-//   gameList.setSlotValue(cnt + 1, cnt)
-
-//   if (intId > 10) {
-//     const vItems = gameList.findValidSlots(5)
-//     console.warn('vItems', vItems)
-//     console.warn('gameList.slots', gameList.slots)
-
-//     clearInterval(intId)
-
-//   }
-//   cnt++
-
-//   console.log(' ', );
-// }, 50)
+let cnt = 0;
 
 const defaultColorStops = [
   { color: '#333136', stop: 40 },
   { color: '#272E32', stop: 100 }
-]
-
-// const invalidSlots = findValidSlots(mockSlots1, 60)
-// const validSlots = mockSlots1.filter(({ id, value }) => !invalidSlots.includes(id));
-
-
-
-// console.warn('invalidSlots, validSlots', invalidSlots, validSlots)
+];
 
 const generateNewRando = () => {
   randomBtn.value = randInt(1, 1000)
@@ -61,16 +32,14 @@ const generateNewRando = () => {
 
 randomBtn.addEventListener('click', e => {
   if (randomBtn.value !== 'RANDO') {
-   return 
+    return
   }
   const rando = generateNewRando()
   const unsetSlotIds = gameList.findNullSlots(rando)
-  // const lowestValid = gameList.findLowestValid(rando)
-if (!unsetSlotIds.length) {
-  randomBtn.value = 'LOSE'
+  if (!unsetSlotIds.length) {
+    randomBtn.value = 'LOSE'
+  }
   
-}
-
   const rows = gameGrid.querySelectorAll('.row')
   
   rows.forEach((el, i) => {
@@ -79,17 +48,23 @@ if (!unsetSlotIds.length) {
   });
   unsetSlotIds.forEach((id, i) => {
     id++
-    // const r = gameGrid.
+
     const r = gameGrid.querySelector(`.row[data-row-number="${id}"]`)
     r.dataset.isOpen = true
   });
 });
 
+const instruEl = document.querySelector('#instructions');
+
 gameGrid.addEventListener('click', e => {
+  if (instruEl.style.display !== 'none') {
+    instruEl.style.display = 'none'
+  }
+  
   const target = e.target
   const targetRow = target.closest('.row')
   const targetCell = targetRow.querySelector('.row-cell')
-
+  
   const rowNumber = +targetRow.dataset.rowNumber
   
   const slotModel = gameList.findById(rowNumber)
@@ -108,9 +83,7 @@ gameGrid.addEventListener('click', e => {
   }
   
   if (randomBtn.value !== 'RANDO') {}
-  
-  
-});
+ });
 
 const generateGrid = (rowCount) => {
   const rows = new Array(rowCount).fill(null)
@@ -126,7 +99,6 @@ const generateGrid = (rowCount) => {
       const rowCell = createDOM('div', {
         dataset: { rowNumber },
         classList: ['row-cell'],
-        // attrs: { contenteditable: true },
       });
       
       const row = createDOM('div', {
