af61d16b00dea2b5e8dc58bd200db896a4e1a467
hamilsauce <jacob.w.hamilton@outlook.com>
2025-09-19 19:12:13 -0500

    update

--- script.js
+++ script.js
@@ -12,73 +12,70 @@ export class UI {
   constructor(root = document.querySelector('#app'), appState = new AppState()) {
     this.root = root;
     this.appState = appState;
-    this.gameGrid = this.getEl('#game-grid')
-    this.randomBtn = this.getEl('#random-button')
-    this.instruEl = this.getEl('#instructions')
-    this.appHeaderCenter = this.getEl('#app-header-center')
-    this.scoreEl = this.getEl('#score-value')
-    this.retriesEl = this.getEl('#retries-value')
-    this.trashButton = this.getEl('#trash-icon')
+    this.gameGrid = this.getEl('#game-grid');
+    this.randomBtn = this.getEl('#random-button');
+    this.instruEl = this.getEl('#instructions');
+    this.appHeaderCenter = this.getEl('#app-header-center');
+    this.scoreEl = this.getEl('#score-value');
+    this.retriesEl = this.getEl('#retries-value');
+    this.trashButton = this.getEl('#trash-icon');
     
-    this.handleRetryClick = this.#handleRetryClick.bind(this)
-    this.handleHeaderClick = this.#handleHeaderClick.bind(this)
+    this.handleRetryClick = this.#handleRetryClick.bind(this);
+    this.handleHeaderClick = this.#handleHeaderClick.bind(this);
     
-    this.appHeaderCenter.addEventListener('click', this.handleHeaderClick)
+    this.appHeaderCenter.addEventListener('click', this.handleHeaderClick);
     this.trashButton.addEventListener('click', this.handleRetryClick);
     
-    let gradientDeg = 0
+    let gradientDeg = 0;
+    
     setInterval(() => {
-      this.root.style.background = `linear-gradient(${gradientDeg}deg, #2E2A32 40%, #23323B 100%)`
-      gradientDeg++
-    }, 16)
+      this.root.style.background = `linear-gradient(${gradientDeg}deg, #2E2A32 40%, #23323B 100%)`;
+      gradientDeg++;
+    }, 16);
   };
   
   // get hasStarted() { return this.root.dataset.gameStarted === 'true' ? true : false; };
-  get isBizarroMode() { return this.#isBizarroMode }
+  get isBizarroMode() { return this.#isBizarroMode; }
   get hasStarted() { return this.root.dataset.gameStarted === 'true' ? true : false; };
   get rows() { return this.getEls('.row'); };
-  get retriesLeft() { return appState.get('retryLimit') - appState.get('retriesUsed') }
+  get retriesLeft() { return appState.get('retryLimit') - appState.get('retriesUsed'); }
   
   #handleHeaderClick(e) {
-    console.log('CLICK')
     if (!this.hasStarted) {
       this.root.dataset.isBizarroMode = true;
-      this.appHeaderCenter.removeEventListener('click', this.handleHeaderClick)
+      this.appHeaderCenter.removeEventListener('click', this.handleHeaderClick);
     }
-    
   };
   
   #handleRetryClick() {
     if (this.retriesLeft === 0) return
-    const used = +appState.get('retriesUsed') + 1
-    this.appState.update({ key: 'retriesUsed', data: +used })
+    const used = +appState.get('retriesUsed') + 1;
+    this.appState.update({ key: 'retriesUsed', data: +used });
     
     if (this.retriesLeft) {
       this.appState.update({
         key: 'currentNumber',
-        data: 0, // randInt(1, 1000)
-      })
+        data: 0,
+      });
     }
   };
   
   generateNewRando() {
-    randomBtn.value = randInt(1, 1000)
-    this.trashButton.dataset.hide = false;
-    return +randomBtn.value
+    this.setButtonValue(randInt(1, 1000));
+    
+    return +randomBtn.value;
   };
   
   setButtonValue(v) {
     if (!v) {
-      randomBtn.value = 'RANDO';
+      this.setButtonValue('RANDO');
     }
     
     else {
       randomBtn.value = v;
     }
     
-    if (isNaN(+v) || !this.retriesLeft) {
-      this.trashButton.dataset.hide = true;
-    }
+    this.trashButton.dataset.hide = (isNaN(+v) || !this.retriesLeft) ? true : false;
   };
   
   getRowById(id) { return this.getEl(`.row[data-row-number="${id}"]`); }
@@ -100,21 +97,21 @@ const appState = new AppState({
 
 
 const app = document.querySelector('#app');
-const appBody = app.querySelector('#app-body')
-const gameGrid = appBody.querySelector('#game-grid')
-const randomBtn = appBody.querySelector('#random-button')
+const appBody = app.querySelector('#app-body');
+const gameGrid = appBody.querySelector('#game-grid');
+const randomBtn = appBody.querySelector('#random-button');
 
 const ui = new UI(app, appState);
 
 appState.on('statechange:currentNumber', (currentNumber) => {
-  ui.setButtonValue(currentNumber)
+  ui.setButtonValue(currentNumber);
 });
 
 appState.on('statechange:retriesUsed', retriesUsed => {
   ui.retriesEl.textContent = `${retriesUsed}/${appState.get('retryLimit')}`;
 });
 
-let gameList
+let gameList;
 
 setTimeout(() => {
   gameList = new GameListModel(gameGrid.children.length)
@@ -128,39 +125,30 @@ const defaultColorStops = [
   { color: '#272E32', stop: 100 }
 ];
 
-const generateNewRando = () => {
-  randomBtn.value = randInt(1, 1000)
-  ui.trashButton.dataset.hide = false;
-  return +randomBtn.value
-};
-
 randomBtn.addEventListener('click', e => {
   if (['START'].includes(randomBtn.value)) {
     app.dataset.gameStarted = true;
   }
   
   if (!['RANDO', 'START'].includes(randomBtn.value)) {
-    return
+    return;
   }
-  const rando = generateNewRando()
+  const rando = ui.generateNewRando()
   const unsetSlotIds = gameList.findNullSlots(rando)
   if (!unsetSlotIds.length) {
-    randomBtn.value = 'LOSE'
-    ui.trashButton.dataset.hide = true;
-    
+    ui.setButtonValue('LOSE');
   }
   
-  const rows = gameGrid.querySelectorAll('.row')
+  const rows = gameGrid.querySelectorAll('.row');
   
   rows.forEach((el, i) => {
-    el.dataset.isOpen = false
+    el.dataset.isOpen = false;
   });
   
   unsetSlotIds.forEach((id, i) => {
-    id++
+    id++;
     
-    const r = gameGrid.querySelector(`.row[data-row-number="${id}"]`)
-    // r.dataset.isOpen = ui.isBizarroMode ? true : false
+    const r = gameGrid.querySelector(`.row[data-row-number="${id}"]`);
     r.dataset.isOpen = true;
   });
 });
@@ -169,34 +157,30 @@ const instruEl = document.querySelector('#instructions');
 
 gameGrid.addEventListener('click', e => {
   if (instruEl.style.display !== 'none') {
-    instruEl.style.display = 'none'
+    instruEl.style.display = 'none';
   }
   
-  // if (instruEl.style.color !== 'transparent') {
-  //   instruEl.style.color = 'transparent'
-  // }
+  const target = e.target;
+  const targetRow = target.closest('.row');
+  const targetCell = targetRow.querySelector('.row-cell');
   
-  const target = e.target
-  const targetRow = target.closest('.row')
-  const targetCell = targetRow.querySelector('.row-cell')
+  const rowNumber = +targetRow.dataset.rowNumber;
   
-  const rowNumber = +targetRow.dataset.rowNumber
+  const slotModel = gameList.findById(rowNumber);
   
-  const slotModel = gameList.findById(rowNumber)
   if (targetRow.dataset.isOpen === 'false' || targetRow.dataset.locked === 'true') {
-    return
+    return;
   }
   
   if (slotModel && !slotModel.isSet && randomBtn.value !== 'RANDO') {
-    slotModel.setValue(+randomBtn.value)
-    targetCell.textContent = randomBtn.value
-    targetRow.classList.add('locked')
+    slotModel.setValue(+randomBtn.value);
+    targetCell.textContent = randomBtn.value;
+    targetRow.classList.add('locked');
     targetRow.dataset.locked = true;
-    randomBtn.value = 'RANDO'
-    ui.trashButton.dataset.hide = true;
+    ui.setButtonValue('RANDO');
     
-    const scoreEl = document.querySelector('#score-value')
-    scoreEl.textContent = `${gameList.slotsWithValues.length}/${gameList.slots.length}`
+    const scoreEl = document.querySelector('#score-value');
+    scoreEl.textContent = `${gameList.slotsWithValues.length}/${gameList.slots.length}`;
   }
   
   if (randomBtn.value !== 'RANDO') {}
@@ -226,7 +210,8 @@ const generateGrid = (rowCount) => {
       
       return row;
     });
-  return rows
+  
+  return rows;
 };
 
 const rows = generateGrid(15);
